7d4607aab13c69664c0122adf4db3cb9c40a451c
Guillaume Gaullier <guillaume.gaullier@gmail.com>
2016-10-10 12:19:58 -0600

    Added backup script

---
+++ backup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Backup script based on rsync.
+
+# Adapted from <https://blog.interlinked.org/tutorials/rsync_time_machine.html>
+# and <https://blog.interlinked.org/tutorials/rsync_addendum.yaml.html>
+
+# Creates incremental snapshots in a manner similar to Apple's Time Machine, but
+# with two advantages: it should work on any system where bash and rsync are
+# installed, and on a Mac it offers the possibility to backup only a certain
+# directory (whereas Time Machine only does full backups).
+
+
+### Configuration: ###
+
+# SRC is the local directory to backup.
+# DEST is the external drive where to store backups.
+# EXCLUDE points to a file containing a list of paths to exclude from backups;
+# by default this file is `$HOME/.rsync/exclude' (see rsync doc for details).
+SRC=$HOME
+DEST=
+EXCLUDE=$HOME/.rsync/exclude
+
+
+### Do not edit after this line. ###
+
+# We cannot backup if the destination directory is not present (typically, this
+# directory would be on an external drive, therefore we cannot assume it's
+# always present).
+if [ ! -d $DEST ]; then
+    echo "Destination directory not found. Please plug your external drive. :-)"
+    exit 0
+fi
+
+# If the EXCLUDE file doesn't exist, subsequent rsync commands will fail,
+# therefore we need to make sure this file exists.
+if [ ! -f $EXCLUDE ]; then
+    touch $EXCLUDE
+fi
+
+# Get current date to name backup folder.
+DATE=`date "+%Y-%m-%d_%H-%M-%S"`
+
+# If this script runs on a Mac, we need the -E option to preserve "extended
+# attributes" in backups (this option apparently doesn't exist in rsync found on
+# other systems).
+if [ `uname` = "Darwin" ]; then
+    echo "Mac detected. Preserving extended attributes."
+    alias rsync="rsync -E"
+fi
+
+# Backup: copy SRC to DEST, preserving all file properties (owner, permissions,
+# modification time, extended attributes, etc.), and taking advantage of the
+# most recent backup (if available) to save disk space.
+
+# If an older backup already exists, we can save disk space with the --link-dest
+# option. Otherwise we proceed with the first backup without this option.
+if [ -L $DEST/latest ]; then
+    rsync -azxP \
+          --human-readable \
+          --log-file=$DEST/$DATE-backup.log \
+          --delete \
+          --delete-excluded \
+          --exclude-from=$EXCLUDE \
+          --link-dest=$DEST/latest \
+          $SRC $DEST/$DATE-backup
+    # Update `latest' symlink to point to most recent backup (i.e. the one that
+    # rsync just created).
+    rm -f $DEST/latest
+    ln -s $DEST/$DATE-backup $DEST/latest
+else
+    rsync -azxP \
+          --human-readable \
+          --log-file=$DEST/$DATE-backup.log \
+          --exclude-from=$EXCLUDE \
+          $SRC $DEST/$DATE-backup
+    # Create `latest' symlink so next backup can detect this first backup and
+    # save disk space using it.
+    ln -s $DEST/$DATE-backup $DEST/latest
+fi
+
