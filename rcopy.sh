a1e01bd1e832165e4015303cda559c9dc5629edd
Guillaume Gaullier <guillaume@gaullier.org>
2017-12-08 14:33:38 -0700

    Cosmetic fixes in rcopy.sh

--- rcopy.sh
+++ rcopy.sh
@@ -6,8 +6,8 @@
 
 ### Configuration: ###
 
-# SRC is the local directory to backup, no trailing slash to designate its content.
-# DEST is the external drive where to store backups, no trailings slash.
+# SRC is the directory to copy, with trailing slash to designate its content.
+# DEST is the destination directory, no trailings slash.
 # By default, SRC and DEST are read from command-line arguments.
 SRC=$1
 DEST=$2
@@ -17,31 +17,31 @@ DEST=$2
 # If no SRC and DEST are provided as command-line arguments, display a short
 # usage guide.
 if [ $# != 2 ]; then
-    echo "Usage: ./rcopy.sh SRC/ DEST"
-    echo "SRC/ is the source directory to backup, trailing slash means copy its content."
-    echo "DEST is the destination directory where to store backups, no trailing slash."
+    echo "Usage: rcopy.sh SRC DEST"
+    echo "SRC is the source directory to copy, no trailing slash."
+    echo "DEST is the destination directory, no trailing slash."
     exit 0
 fi
 
-# We cannot backup if the destination directory is not present (typically, this
+# Sanity check: if the source directory doesn't exist, we need to tell the user.
+if [ ! -d $SRC ]; then
+    echo "Source directory not found."
+    echo "Please specify an existing directory to copy."
+    exit 0
+fi
+
+# We cannot copy if the destination directory is not present (typically, this
 # directory would be on an external drive, therefore we cannot assume it's
 # always present).
 if [ ! -d $DEST ]; then
     echo "Destination directory not found."
-    echo "Please plug your external drive. :-)"
+    echo "Please plug your external drive or check you typed the correct path. :-)"
     exit 0
 fi
 
-# Sanity check: if the local directory doesn't exist, we need to tell the user.
-if [ ! -d $SRC ]; then
-    echo "Local directory not found."
-    echo "Please specify an existing directory to backup."
-    exit 0
-fi
-
-# Copy SRC/ contents to DEST, preserving all file properties (owner,
-# permissions, modification time, etc.). Symbolic links are copied as links,
-# i.e. the file or directory they point to is not copied.
+# Copy SRC to DEST, preserving all file properties (owner, permissions,
+# modification time, etc.). Symbolic links are copied as links, i.e. the file or
+# directory they point to is not copied.
 rsync --recursive \
       --links \
       --perms \
