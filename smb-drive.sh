3c7957909ba841a415e06bff82c2bf3ecf5ae59c
Guillaume Gaullier <guillaume@gaullier.org>
2022-06-21 11:28:56 +0200

    smb-drive.sh can now connect in read-only or read/write mode

--- smb-drive.sh
+++ smb-drive.sh
@@ -14,7 +14,7 @@ SERVER=
 DIR=
 
 usage () {
-	echo "Usage: smb-drive.sh [connect|co|disconnect|dis]"
+	echo "Usage: smb-drive.sh [connect|co|disconnect|dis] [rw]"
 	exit 1
 }
 
@@ -25,19 +25,28 @@ connect () {
 		mkdir -p $MNT
 	fi
 
-	# Connect
-	sudo mount -t cifs -o username=$USERNAME,password=$PASSWORD,$DOMAIN //$SERVER/$DIR $MNT
+	# Connect in read/write mode if the argument to this function is "rw",
+	# otherwise connect in read-only mode
+	case $1 in
+		"rw")
+			echo "Connecting in read/write mode. Be careful!"
+			sudo mount -t cifs -o uid=$USER,username=$USERNAME,password=$PASSWORD,$DOMAIN //$SERVER/$DIR $MNT;;
+		*)
+			sudo mount -t cifs -o username=$USERNAME,password=$PASSWORD,$DOMAIN //$SERVER/$DIR $MNT;;
+	esac
 }
 
 disconnect () {
 	sudo umount $MNT
 }
 
+# Pass second command line argument to connect
+# so it can decide between read-only and read/write modes
 case $1 in
 	"connect")
-		connect;;
+		connect $2;;
 	"co")
-		connect;;
+		connect $2;;
 	"disconnect")
 		disconnect;;
 	"dis")
